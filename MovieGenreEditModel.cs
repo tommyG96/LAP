306263327156c9454ff14a348ba1e88fe6607034
tommyG96 <56869772+tommyG96@users.noreply.github.com>
2021-03-06 21:49:56 +0100

    Add files via upload

---
+++ MovieGenreEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class MovieGenreEditModel
+    {
+        public int Gen_Id { get; set; }
+        public string Gen_name { get; set; }
+    }
+}
